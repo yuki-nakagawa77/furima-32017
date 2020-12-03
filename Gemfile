240536d51516d6da1924804ef22d816c705873af
yuki-nakagawa77 <tuna27kan789@gmail.com>
2020-12-03 15:21:52 +0900

    商品出品機能

--- Gemfile
+++ Gemfile
@@ -61,3 +61,8 @@ end
 
 gem 'devise'
 gem 'pry-rails'
+
+gem 'active_hash'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,25 @@
 class Item < ApplicationRecord
-  #belongs_to :user
-  #has_one    :orders_history
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :day
+  
 
-  #validates :user, presence: true
-  #validates :product_name, presence: true
-  #validates :explanation, presence: true
-  #validates :category_id, presence: true
-  #validates :status_id, presence: true
-  #validates :burden_id, presence: true
-  #validates :area_id, presence: true
-  #validates :day_id, presence: true
-  #validates :price, presence: true
+  belongs_to :user
+  has_one    :orders_history
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :explanation
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :burden_id, numericality: { other_than: 1 }
+    validates :area_id, numericality: { other_than: 1 }
+    validates :day_id, numericality: { other_than: 1 }
+    validates :price, inclusion: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
+  end
 end
