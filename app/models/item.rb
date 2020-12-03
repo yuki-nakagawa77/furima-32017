13197ce8e6f1cff77236398d5d1a0f2fb4015ba5
yuki-nakagawa77 <tuna27kan789@gmail.com>
2020-12-03 16:18:59 +0900

    商品出品機能　修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,15 +11,23 @@ class Item < ApplicationRecord
   has_one    :orders_history
   has_one_attached :image
 
-  with_options presence: true, numericality: { other_than: 1 } do
+  with_options presence: true do
     validates :image
     validates :product_name
     validates :explanation
-    validates :category_i 
+    validates :category_id
     validates :status_id
     validates :burden_id
     validates :area_id
     validates :day_id
     validates :price, inclusion: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
+    
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :burden_id
+      validates :area_id
+      validates :day_id
+    end
   end
 end
