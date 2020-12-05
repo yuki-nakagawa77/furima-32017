42f706c85551aa7e384b2b7400bb1a5830890730
yuki-nakagawa77 <tuna27kan789@gmail.com>
2020-12-05 12:45:54 +0900

    商品情報編集機能　完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,20 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id), method: :get
+    else
+      render :edit
+    end
+  end
+
+
   private
 
   def item_params
@@ -31,3 +45,4 @@ class ItemsController < ApplicationController
   end
 
 end
+
