1e80f4779f619e44cec98435ea572d941ce71c81
yuki-nakagawa77 <tuna27kan789@gmail.com>
2020-12-05 15:04:23 +0900

    商品情報編集機能　修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :date, only: [:show, :edit, :update]
+  before_action :data, only: [:show, :edit, :update]
 
   def new
     @item = Item.new
