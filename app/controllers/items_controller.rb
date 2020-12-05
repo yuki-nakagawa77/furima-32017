21e2ca1cd8bb66ddc6a6f7d3b3837ea9d636e80c
yuki-nakagawa77 <tuna27kan789@gmail.com>
2020-12-05 14:46:31 +0900

    商品情報編集機能　修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :date, only: [:create, :show, :edit, :update]
+  before_action :date, only: [:show, :edit, :update]
 
   def new
     @item = Item.new
@@ -12,6 +12,7 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
     if  @item.valid?
         @item.save 
         redirect_to root_path
@@ -37,9 +38,6 @@ class ItemsController < ApplicationController
     end
   end 
 
-  def data
-  @item = Item.new(item_params)
-  end 
 
 
 
@@ -48,6 +46,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :product_name, :explanation, :category_id, :status_id, :burden_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
   end
+
+  def data
+    @item= Item.find(params[:id])
+  end 
 
 end
 
